25acf7851d85ac2be9beb9d385b479ee456369bf
Anas3603 <mohammadanasnisar@gmail.com>
2025-08-15 01:14:13 +0530

    correct bugs in contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,27 +12,34 @@ const Contact = () => {
     <>
       <div className="container contact" id="contact">
         <h1>CONTACT ME</h1>
-        <div className="contact-icon"
-        data-aos="zoom-in-up"
-      data-aos-duration="1000"
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
         >
-          <a href=""  className="items">
-            <FaInstagram className="icons"/>
+          <a
+            href="https://www.instagram.com/m.o.h.d._.a.n.a.s?utm_source=qr"
+            className="items"
+          >
+            <FaInstagram className="icons" />
           </a>
-          <a href=""  className="items">
-            <FaFacebook className="icons"/>
+          <a href="https://www.facebook.com/mohammadanasnisar" className="items">
+            <FaFacebook className="icons" />
           </a>
-          <a href=""  className="items">
-            <FaLinkedin className="icons"/>
+          <a
+            href="https://www.linkedin.com/in/anas-nisar-75108a2ba/"
+            className="items"
+          >
+            <FaLinkedin className="icons" />
           </a>
-          <a href=""  className="items">
-            <FaSquareXTwitter className="icons"/>
+          <a href="https://x.com/m_o_h_d_a_n_a_s" className="items">
+            <FaSquareXTwitter className="icons" />
           </a>
-          <a href=""  className="items">
-            <FaGithub className="icons"/>
+          <a href="https://github.com/Anas3603" className="items">
+            <FaGithub className="icons" />
           </a>
-          <a href="mailto:mohammadanasnisar@gmail.com"  className="items">
-            <IoIosMail className="icons"/>
+          <a href="mailto:mohammadanasnisar@gmail.com" className="items">
+            <IoIosMail className="icons" />
           </a>
         </div>
       </div>
